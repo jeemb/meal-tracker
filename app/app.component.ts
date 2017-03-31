c43b9fdb31766046c751aad017ecc5716961be09
Jennifer Beem <jenniferbeem@gmail.com>
2017-03-31 08:57:16 -0700

    add food-list component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,20 +1,21 @@
 import { Component } from '@angular/core';
+import { Food } from './food.model';
 
 @Component({
   selector: 'app-root',
   template: `
-  <div class="container">
+  <div class='container'>
     <h1>Meal Tracker</h1>
-    <food-list></food-list>
+    <food-list [foods]="foods" (clickSender)="selectFood($event)"></food-list>
   </div>
   `
 })
 
 export class AppComponent {
-  food = Food[] = [
+  foods: Food[] = [
     new Food('coffee', '200', 'without milk this morning'),
     new Food('breakfast sandwich', '250', 'bacon cheese and egg'),
     new Food('pasta', '350', 'with sauce')
-  ];
+  ]
 
 }
---
+++ app/food-list.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Food } from './food.model';
+
+@Component({
+selector: 'food-list',
+template: `
+<div>
+`
+})
+
+export class FoodListComponent {
+  
+}
