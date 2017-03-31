d2f58f0dec62345a10fe6f87fe0d231856e5f250
Jennifer Beem <jenniferbeem@gmail.com>
2017-03-31 08:35:07 -0700

    add information to initial app.component.ts file

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,10 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <h1>My First Angular 2 App</h1>
+  <div class="container">
+    <h1>Meal Tracker</h1>
+    <food-list></food-list>
+  </div>
   `
 })
 
 export class AppComponent {
+  food = Food[] = [
+    new Food('coffee', '200', 'without milk this morning'),
+    new Food('breakfast sandwich', '250', 'bacon cheese and egg'),
+    new Food('pasta', '350', 'with sauce')
+  ];
 
 }
