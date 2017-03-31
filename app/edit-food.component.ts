e333283e95b0b039321f11f0499b77f33d6e0099
Jennifer Beem <jenniferbeem@gmail.com>
2017-03-31 09:46:48 -0700

    add ngIf statement to show edit form

--- app/edit-food.component.ts
+++ app/edit-food.component.ts
@@ -4,10 +4,23 @@ import { Food } from './food.model';
 @Component({
   selector: 'edit-food',
   template: `
+  <div *ngIf="selectedFood">
+    <label>Name:</label>
+    <input [(ngModel)]="selectedFood.name">
+    <label>Calories</label>
+    <input [(ngModel)]="selectedFood.calories">
+    <label>Details</label>
+    <input [(ngModel)]="selectedFood.details">
+    <button (click)="finishedEditing()">Done</button>
+  </div>
   `
 })
 
 export class EditKegComponent {
+    @Input() selectedKeg: Food[];
+    @Output() clickSender = new EventEmitter();
 
-
+    finishedEditing() {
+      this.clickSender.emit();
+    }
 }
